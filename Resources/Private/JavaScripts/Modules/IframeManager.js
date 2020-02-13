cfe22f39f6a84693dc578cf7a93e613d7b3b317b
Patrick Fischer <patrick.fischer@in2code.de>
2020-02-13 16:03:49 +0100

    [FEATURE] - added Description Text for IframeConsent   & submit button
    https://projekte.in2code.de/issues/38246

--- Resources/Private/JavaScripts/Modules/IframeManager.js
+++ Resources/Private/JavaScripts/Modules/IframeManager.js
@@ -1,6 +1,5 @@
 class IframeManager {
 	constructor() {
-
 	}
 
 	/**
@@ -15,6 +14,16 @@ class IframeManager {
 
 		var newElement = document.createElement("div");
 		newElement.classList.add("in2iframe-container");
+		newElement.innerHTML = "<div>" +
+			"<p>" +
+			"Zum Abspielen dieses IFrames müssen Sie auf den Link \"IFrame\n" +
+			"starten\" klicken. Wir möchten Sie darauf hinweisen, dass durch den Start\n" +
+			"des IFrames Daten an Drittanbieter übermittelt werden" +
+			"</p>" +
+			"<button class='in2iframe-button'>" +
+			"IFrame starten" +
+			"</button>" +
+			"</div>";
 		newElement.setAttribute("data-in2iframeconsent-id", id);
 
 		parentElement.appendChild(newElement);
