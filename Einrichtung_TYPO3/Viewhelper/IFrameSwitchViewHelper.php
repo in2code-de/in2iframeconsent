6cc1d85f544906c2a86d6b917791950ebc7b15b6
Patrick Fischer <patrick.fischer@in2code.de>
2020-03-13 16:23:21 +0100

    [FEATURE] - Viewhlerper, which should be used for our projects, is included here in the repo - Partials, which are used by the viewhelper, added to the repo - Example HTML added to the Viewhelper call
    Related: https://projekte.in2code.de/issues/39403

---
+++ Einrichtung_TYPO3/Viewhelper/IFrameSwitchViewHelper.php
@@ -0,0 +1,113 @@
+<?php
+declare(strict_types=1);
+
+namespace In2code\In2template\ViewHelpers;
+
+use TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHelper;
+
+/**
+ * Class OverrideSubheaderPropertyHelper
+ */
+class IFrameSwitchViewHelper extends AbstractViewHelper
+{
+    /**
+     * @var bool
+     */
+    protected $escapeOutput = false;
+
+    /**
+     * @return void
+     */
+    public function initializeArguments()
+    {
+        $this->registerArgument('content', 'string', 'consent markup', false);
+    }
+
+    /**
+     * @return mixed|string
+     */
+    public function render()
+    {
+        if ($this->isIframeExisting()) {
+            return $this->replace();
+        } else {
+            return $this->renderChildren();
+        }
+    }
+
+    /**
+     * @return string
+     */
+    protected function replace(): string
+    {
+        $dom = new \DOMDocument();
+        @$dom->loadHTML(
+            $this->wrapHtmlWithMainTags($this->renderChildren())
+            , LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD
+        );
+
+        $content = new \DOMDocument();
+        @$content->loadHTML(
+            $this->wrapHtmlWithMainTags($this->arguments['content'])
+            , LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD
+        );
+
+        /** @var \DOMElement $iframe */
+        if ($dom->getElementsByTagName('iframe')->length > 0) {
+            foreach ($dom->getElementsByTagName('iframe') as $iframe) {
+                $attributes = [];
+                /** @var \DOMAttr $attribute */
+                foreach ($iframe->attributes as $attribute) {
+                    $attributes[$attribute->nodeName] = $attribute->nodeValue;
+                }
+
+                $div = $dom->createElement('div');
+                $div->setAttribute('class', 'iframeswitch-container');
+                foreach ($attributes as $attributeKey => $attribute) {
+                    $div->setAttribute('data-iframeswitch-' . $attributeKey, $attribute);
+                }
+
+                $div->appendChild($dom->importNode($content->documentElement, true));
+
+                $iframe->parentNode->replaceChild($div, $iframe);
+            }
+        }
+
+        return $this->stripMainTagsFromHtml($dom->saveHTML());
+    }
+
+    /**
+     * @return bool
+     */
+    protected function isIframeExisting(): bool
+    {
+        return stristr($this->renderChildren(), '<iframe') !== false;
+    }
+
+    /**
+     * Wrap html with "<?xml encoding="utf-8" ?><html><body>|</body></html>"
+     *
+     *  This is a workarround for HTML parsing and wrting with \DOMDocument()
+     *      - The html and body tag are preventing strange p-tags while using LIBXML_HTML_NOIMPLIED
+     *      - The doctype declaration allows us the usage of umlauts and special characters
+     *
+     * @param string $html
+     * @return string
+     */
+    protected function wrapHtmlWithMainTags(string $html): string
+    {
+        return '<?xml encoding="utf-8" ?><html><body>' . $html . '</body></html>';
+    }
+
+    /**
+     * Remove tags <?xml encoding="utf-8" ?><html><body></body></html>
+     * This function is normally used after wrapHtmlWithMainTags
+     *
+     * @param string $html
+     * @return string
+     */
+    protected function stripMainTagsFromHtml(string $html): string
+    {
+        return str_replace(['<html>', '</html>', '<body>', '</body>', '<?xml encoding="utf-8" ?>'], '', $html);
+    }
+}
